77b99bb329a88d7c9be54d7598cda458fa51c0fd
Aaron Lista <aarondavidlista@gmail.com>
2024-02-27 14:57:01 -0400

    se agrego una validacion a review para que solo sea integer

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :list
 
   validates :content, presence: true
-  validates :rating, presence: true, comparison: { less_than_or_equal_to: 5 }
+  validates :rating, presence: true, numericality: { only_integer: true }, comparison: { less_than_or_equal_to: 5 }
 end
